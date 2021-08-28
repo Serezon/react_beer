24fee96ee0cc43174981d667d536f517b38c1bd6
darkspice <darkspice@yandex.ru>
2021-08-28 11:23:12 +0400

    feat: add CardsContainer

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Search from '../Search'
-import Card from '../Card'
+import CardsContainer from '../CardsContainer'
 import './style.css'
 
 export default function App(props) {
@@ -28,40 +28,11 @@ export default function App(props) {
 		setOptions(newUrl)
 	}
 
-	function makingCards() {
-		let cards = beer.slice()
-		if (filter) {
-			cards = cards.filter(item => {
-				let id = String(item.id)
-				let abv = String(item.abv)
-				let ibu = String(item.ibu)
-				let ebc = String(item.ebc)
-				let yeast = item.ingredients.yeast
-				let brewed = item.first_brewed
-				let hops = item.ingredients.hops.some(item => item.name === filter.hops)
-				let malt = item.ingredients.malt.some(item => item.name === filter.malt)
-				if (
-					(id === filter.id || filter.id === '') &&
-					(abv === filter.abv || filter.abv === '') &&
-					(ibu === filter.ibu || filter.ibu === '') &&
-					(ebc === filter.ebc || filter.ebc === '') &&
-					(yeast === filter.yeast || filter.yeast === '') &&
-					(brewed === filter.brewed || filter.brewed === '') &&
-					(hops || filter.hops === '') &&
-					(malt || filter.malt === '')
-				) {
-					return true
-				}
-			})
-		}
-		cards = cards.map(item => <Card key={item.id} beer={item} />)
-		return cards
-	}
 	return (
 		<div>
 			<Search beer={beer} filter={applyFilter} options={applyOptions} />
 			<div className="card-container">
-				{makingCards()}
+				<CardsContainer filter={filter} beer={beer}/>
 			</div>
 			<div className="page-navigation">
 			</div>
